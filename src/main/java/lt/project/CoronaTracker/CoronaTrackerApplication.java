cf549f63882311b38995d4e2824e9b5b828a1eb9
Povilas <zdanis.povilas@gmail.com>
2022-02-12 10:10:00 +0200

    Initial commit

---
+++ src/main/java/lt/project/CoronaTracker/CoronaTrackerApplication.java
@@ -0,0 +1,14 @@
+package lt.project.CoronaTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class CoronaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaTrackerApplication.class, args);
+	}
+
+}
